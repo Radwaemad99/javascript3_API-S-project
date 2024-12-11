d4e4581c90edc3f8370aa62587a9c87688bdb13e
radwa emad <Es-RadwaEmad2021@alexu.edu.eg>
2024-12-11 14:32:16 +0200

    my javascript 3 and api's project

--- task2.js
+++ task2.js
@@ -1,13 +1,13 @@
 // Sample data (replace with your API data)
 const data = [
-    { id: 1, name: "radwa emad eldin", age: 25, job: "cms engineer" },
-    { id: 2, name: "abdelrhman emad eldin", age: 27, job: "doctor" },
-    { id: 3, name: "mohamed emad eldin", age: 29, job: "teacher"},
-    { id: 4, name: "hamza ahmed", age: 2, job: "children" },
-    { id: 5, name: "manal abdelaziz", age: 55, job: "teacher" },
-    { id: 6, name: "rana mohamed", age: 26, job: "accountant" },
-    { id: 7, name: "salmaa nageeb", age: 24, job: "web developer" },
-    { id: 8, name: "nada abobakr omar", age: 27, job: "testing engineer" },
+    { id: 1, name: "radwa emad eldin", age: 25, job: "cms engineer", salary: 10000 },
+    { id: 2, name: "abdelrhman emad eldin", age: 27, job: "doctor", salary: 8000 },
+    { id: 3, name: "mohamed emad eldin", age: 29, job: "teacher", salary: 3000},
+    { id: 4, name: "hamza ahmed", age: 2, job: "children", salary: 0 },
+    { id: 5, name: "manal abdelaziz", age: 55, job: "teacher", salary:9000 },
+    { id: 6, name: "rana mohamed", age: 26, job: "accountant", salary: 7000 },
+    { id: 7, name: "salmaa nageeb", age: 24, job: "web developer", salary: 12000 },
+    { id: 8, name: "nada abobakr omar", age: 27, job: "testing engineer", salary:10000 },
     
 ];
 
@@ -22,12 +22,14 @@ function displayData(data) {
         let nameCell = row.insertCell(1);
         let ageCell = row.insertCell(2);
         let jobCell =row.insertCell(3);
+        let salaryCell = row.insertCell(4);
        
 
         idCell.textContent = item.id;
         nameCell.textContent = item.name;
         ageCell.textContent = item.age;
         jobCell.textContent= item.job
+        salaryCell.textContent= item.salary;
 
       
     });
